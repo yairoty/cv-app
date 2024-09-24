83f62f386731fa42af3b19432b3e19c31d8a6465
yair-coder <yair@cyolo.io>
2024-09-24 13:35:31 +0100

    env file

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -10,10 +10,10 @@ export interface EnvVariables {
 export const getEnv = (): EnvVariables => {
   return {
     userFullName: import.meta.env.VITE_USER_FULL_NAME || 'my full name',
-    phone: import.meta.env.VITE_API_KEY || '+123 4567890',
-    whatsapp: import.meta.env.VITE_SECRET_KEY || '+123 4567890',
-    email: import.meta.env.VITE_SECRET_KEY || 'user.mail@mail.com',
-    linkedin: import.meta.env.VITE_SECRET_KEY || 'my-linkedin-url.com',
+    phone: import.meta.env.VITE_PHONE || '+123 4567890',
+    whatsapp: import.meta.env.VITE_WHATSAPP || '+123 4567890',
+    email: import.meta.env.VITE_EMAIL || 'user.mail@mail.com',
+    linkedin: import.meta.env.VITE_LINKEDIN || 'my-linkedin-url.com',
     github: import.meta.env.VITE_GITHUB || 'my-github-url.com',
   };
 };
